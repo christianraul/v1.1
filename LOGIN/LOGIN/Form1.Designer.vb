af05691c3895d8f11501fa0fff961b2be33d7986
Christian <cristian_raul_2@hotmail.com>
2018-10-27 21:42:17 -0500

    inicio

---
+++ LOGIN/LOGIN/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtnum1 = New System.Windows.Forms.TextBox()
+        Me.txtnum2 = New System.Windows.Forms.TextBox()
+        Me.txtresul = New System.Windows.Forms.Label()
+        Me.btncalcular = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "numero1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(47, 126)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "numero2:"
+        '
+        'txtnum1
+        '
+        Me.txtnum1.Location = New System.Drawing.Point(113, 81)
+        Me.txtnum1.Name = "txtnum1"
+        Me.txtnum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum1.TabIndex = 2
+        '
+        'txtnum2
+        '
+        Me.txtnum2.Location = New System.Drawing.Point(113, 118)
+        Me.txtnum2.Name = "txtnum2"
+        Me.txtnum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum2.TabIndex = 3
+        '
+        'txtresul
+        '
+        Me.txtresul.AutoSize = True
+        Me.txtresul.Location = New System.Drawing.Point(156, 192)
+        Me.txtresul.Name = "txtresul"
+        Me.txtresul.Size = New System.Drawing.Size(0, 13)
+        Me.txtresul.TabIndex = 4
+        '
+        'btncalcular
+        '
+        Me.btncalcular.Location = New System.Drawing.Point(138, 156)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btncalcular.TabIndex = 5
+        Me.btncalcular.Text = "CALCULAR"
+        Me.btncalcular.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btncalcular)
+        Me.Controls.Add(Me.txtresul)
+        Me.Controls.Add(Me.txtnum2)
+        Me.Controls.Add(Me.txtnum1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtnum1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtnum2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtresul As System.Windows.Forms.Label
+    Friend WithEvents btncalcular As System.Windows.Forms.Button
+
+End Class
---
+++ LOGIN/LOGIN/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
+        Dim a, b, res As Integer
+        a = (txtnum1.Text)
+        b = (txtnum2.Text)
+        res = a + b
+        txtresul.Text = res
+
+    End Sub
+End Class
